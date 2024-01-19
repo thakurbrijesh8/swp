0717da06ba99e8963506ec4512f56cdbe867f9d4
Solanki Vishal <solanki.vishal@supportgov.in>
2024-01-19 13:25:43 +0530

    - FRF for NA

--- db/18012024_feedback_rating.sql
+++ db/18012024_feedback_rating.sql
@@ -94,3 +94,8 @@ ALTER TABLE `filmshooting`
 ADD `rating` tinyint(1) NOT NULL AFTER `query_status`,
 ADD `feedback` varchar(200) COLLATE 'utf8_general_ci' NOT NULL AFTER `rating`,
 ADD `fr_datetime` datetime NOT NULL AFTER `feedback`;
+
+ALTER TABLE `na`
+ADD `rating` tinyint(1) NOT NULL AFTER `query_status`,
+ADD `feedback` varchar(200) COLLATE 'utf8_general_ci' NOT NULL AFTER `rating`,
+ADD `fr_datetime` datetime NOT NULL AFTER `feedback`;
--- js/project/na.js
+++ js/project/na.js
@@ -71,6 +71,9 @@ Na.listView = Backbone.View.extend({
         if (rowData.query_status != VALUE_ZERO) {
             rowData.show_query_btn = true;
         }
+        if (rowData.status == VALUE_FIVE || rowData.status == VALUE_SIX) {
+            rowData.show_fr_btn = true;
+        }
         return naActionTemplate(rowData);
     },
     loadNaData: function (sDistrict, sStatus) {
@@ -81,7 +84,8 @@ Na.listView = Backbone.View.extend({
 
         var searchData = dashboardNaviationToModule(sDistrict, sStatus);
         var tempRegNoRenderer = function (data, type, full, meta) {
-            return regNoRenderer(VALUE_FOURTY, data);
+            return regNoRenderer(VALUE_FOURTY, data)
+                    + getFRContainer(VALUE_FOURTY, data, full.rating, full.fr_datetime);
         };
         var that = this;
         Na.router.navigate('na');
